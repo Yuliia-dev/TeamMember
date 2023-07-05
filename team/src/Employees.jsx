707e6face850b8a9b45a3eb3556bd2ed833a6d2f
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-07-05 18:34:42 +0300

    styled cards

--- team/src/Employees.jsx
+++ team/src/Employees.jsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
-import team from './team.json';
-import maleProfile from './images/maleProfile.jpg';
-import femaleProfile from './images/femaleProfile.jpg'
+import team from "./team.json";
+import maleProfile from "./images/maleProfile.jpg";
+import femaleProfile from "./images/femaleProfile.jpg";
 
 const Employees = () => {
-  const [employees, setEmployees] = useState(team)
+  const [employees, setEmployees] = useState(team);
 
   return (
     <main className="container">
-      <div class="row ">
-        <div class=" col-8">
-      {employees.map((employee) => (
-        <div key={employee.id}>
-          <p>{employee.fullName}</p>
-          {employee.gender === 'male' ? (
-            <img src={maleProfile} alt="Male Profile" />
-          ) : (
-            <img src={femaleProfile} alt="Female Profile" />
-          )}
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <div class="card-collection">
+          {employees.map((employee) => (
+            <div key={employee.id} className="card">
+              {employee.gender === "male" ? (
+                <img
+                  src={maleProfile}
+                  alt="Male Profile"
+                  className="card-img-top"
+                />
+              ) : (
+                <img src={femaleProfile} alt="Female Profile" />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">Full name: {employee.fullName}</h5>
+                <p className="card-text">
+                  <b>Designation:</b> {employee.designation}
+                </p>
+              </div>
+            </div>
+          ))}
+          </div>
         </div>
-      ))}</div></div>
+      </div>
     </main>
   );
 };
