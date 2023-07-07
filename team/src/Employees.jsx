6c80beab3a0e031d74dd04e796372b9d5d52a125
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-07-07 21:28:41 +0300

    add select

--- team/src/Employees.jsx
+++ team/src/Employees.jsx
@@ -4,15 +4,35 @@ import maleProfile from "./images/maleProfile.jpg";
 import femaleProfile from "./images/femaleProfile.jpg";
 
 const Employees = () => {
+  const [selectedTeam, setSelectedTeam] = useState('TeamB');
   const [employees, setEmployees] = useState(team);
+
+  function handleTeamSelection(event) {
+  setSelectedTeam(event.target.value)
+  }
+
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.target.id) ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' } : { ...employee, TeamName: selectedTeam } : employee)
+    setEmployees(transformedEmployees)
+  }
 
   return (
     <main className="container">
+       <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-6">
+          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelection}>
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+           
+        </select>
+        </div></div>
       <div class="row justify-content-center mt-3 mb-3">
         <div class="col-8">
           <div class="card-collection">
           {employees.map((employee) => (
-            <div key={employee.id} className="card">
+            <div key={employee.id} className="card m-2" style={{ cursor: 'pointer'}} onClick={handleEmployeeCardClick}>
               {employee.gender === "male" ? (
                 <img
                   src={maleProfile}
@@ -20,7 +40,7 @@ const Employees = () => {
                   className="card-img-top"
                 />
               ) : (
-                <img src={femaleProfile} alt="Female Profile" />
+                <img src={femaleProfile} alt="Female Profile" className="card-img-top"/>
               )}
               <div className="card-body">
                 <h5 className="card-title">Full name: {employee.fullName}</h5>
