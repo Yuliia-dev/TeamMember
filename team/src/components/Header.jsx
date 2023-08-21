a659c492ad4bd2903b55c084b66971189eb5de98
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-08-21 14:24:49 +0300

    fix amount

--- team/src/components/Header.jsx
+++ team/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ selectedTeam, teamMemberCounts, employees }) => {
       <div className="row justify-content-center mt-3 mb-4">
         <div className="col-8">
           <h1>Team Member Allocation</h1>
-          <h5> Amount of employees : {employees.length + 1} </h5>
+          <h5> Amount of employees : {employees.length} </h5>
           <h3>
             {selectedTeam} has {teamMemberCounts}{" "}
             {teamMemberCounts === 1 ? "member" : "members"}
