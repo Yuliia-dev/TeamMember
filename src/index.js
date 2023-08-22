1653274aad01944eb428158d18d4cbe3cd691085
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-08-22 15:09:22 +0300

    repl photo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/TeamMember/">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
